68dd53d7046a97ed4d40d9d60cf9489a424e7a78
Vitalii Nemyrovskyi <vitalii.nemyrovskyi@gmail.com>
2026-07-05 00:08:24 +0300

    refactor: migrate all store action types to dot-notation namespacing

--- case17-bigstore/src/store/user.actions.ts
+++ case17-bigstore/src/store/user.actions.ts
@@ -1,6 +1,6 @@
 // Action types for the user feature slice.
-export const USER_LOGIN = "user/login";
-export const USER_LOGOUT = "user/logout";
-export const USER_UPDATEPROFILE = "user/updateProfile";
-export const USER_VERIFYEMAIL = "user/verifyEmail";
-export const USER_DELETEACCOUNT = "user/deleteAccount";
+export const USER_LOGIN = "user.login";
+export const USER_LOGOUT = "user.logout";
+export const USER_UPDATEPROFILE = "user.updateProfile";
+export const USER_VERIFYEMAIL = "user.verifyEmail";
+export const USER_DELETEACCOUNT = "user.deleteAccount";
